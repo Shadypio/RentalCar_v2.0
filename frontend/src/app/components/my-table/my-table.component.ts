051078d74b804a4008a994bb9860b5ec353cb444
Shadypio <enzuccioamendola@gmail.com>
2023-02-10 12:56:08 +0100

    Added car service and some data

--- frontend/src/app/components/my-table/my-table.component.ts
+++ frontend/src/app/components/my-table/my-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MyTableConfig } from './config/my-table-config';
 
 @Component({
   selector: 'app-my-table',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class MyTableComponent implements OnInit {
 
   constructor() { }
+  @Input () tableConfig? : MyTableConfig;
 
   ngOnInit(): void {
   }
---
+++ frontend/src/app/services/car.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Input } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  constructor() { }
+
+  // a simple car has ID, license plate, brand, model, year, category
+  @Input () data : any[] = [[7, "AT161FL", "Panda", "2 serie", 2014, "Automobile"],
+                            [2, "CG272GM", "Renault", "Turbo", 2010 ,"Autocarro"],
+                            [3, "BQ383KN", "Volkswagen", "Express", 2015,"Camper"],
+                            [8, "FN494JO", "Opel", "Max",2013,"Automobile"],
+                            [5, "EM515KP", "BMW", "New", 2022,"Camper"],
+                            ]
+
+  getProducts()  {
+    return this.data;
+
+  }
+}
