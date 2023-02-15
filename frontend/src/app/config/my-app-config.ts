14bface5f7e7bbcf1223799e344aa1543b50c37b
Shadypio <enzuccioamendola@gmail.com>
2023-02-15 17:48:31 +0100

    Changed Okta user

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
   oidc: {
-    clientId: '0oa8cb2gzwPPXgDWJ5d7',
-    issuer: 'https://dev-76613192.okta.com/oauth2/default',
+    clientId: '0oa8cg131gdT0NlaO5d7',
+    issuer: 'https://dev-59815294.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
     /** profile is user's profile: first name, last name, phone etc */
