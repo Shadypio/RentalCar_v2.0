333dba359206f567871f8a85d932157f8f4ac3ba
Shadypio <enzuccioamendola@gmail.com>
2023-02-22 17:17:50 +0100

    Added Customer, Rental and Role repositories classes

---
+++ backend/rental-car/src/main/java/com/mygroup/rentalcar/dao/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.mygroup.rentalcar.dao;
+
+import com.mygroup.rentalcar.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ backend/rental-car/src/main/java/com/mygroup/rentalcar/dao/RentalRepository.java
@@ -0,0 +1,10 @@
+package com.mygroup.rentalcar.dao;
+
+
+import com.mygroup.rentalcar.entity.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+}
