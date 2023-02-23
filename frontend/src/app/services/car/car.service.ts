59f4612d76f21a3d45dfeb243b7cfe8b795a8eb5
Shadypio <enzuccioamendola@gmail.com>
2023-02-23 15:10:54 +0100

    Corrected methods for Car Service

--- frontend/src/app/services/car/car.service.ts
+++ frontend/src/app/services/car/car.service.ts
@@ -1,4 +1,8 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable, Input } from '@angular/core';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { Car } from 'src/app/common/car/car';
@@ -7,8 +11,6 @@ import { Car } from 'src/app/common/car/car';
   providedIn: 'root',
 })
 export class CarService {
-
-
   apiUrl: string = 'http://localhost:3000/cars';
   baseUrl = 'http://localhost:8080/api/cars';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
@@ -16,53 +18,37 @@ export class CarService {
   static id: number = 100;
   // private baseUrl = "http://localhost:8080/api/cars"
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   // Show lists of item
   getCars(): Observable<any> {
-    /*
-    return this.httpClient.get(this.apiUrl).pipe(
-      catchError(this.handleError)
-    );*/
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-      map(response => response._embedded.cars)
-    );
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.cars));
+  }
+
+  getCarById(id: any): Observable<any> {
+    const carUrl = `${this.baseUrl}/${id}`;
+    return this.httpClient.get<Car>(carUrl);
   }
 
   // Create new item
-  getCarById(id: any): Observable<any> {
-    /*
-    return this.httpClient.get(`${this.apiUrl}/${id}`).pipe(
-      catchError(this.handleError)
-    );*/
-    const carUrl = `${this.baseUrl}/${id}`
-    return this.httpClient.get<Car>(carUrl)
-  }
-
-  create(data: any): Observable<any> {/*
-    return this.httpClient.post(this.apiUrl, data).pipe(
-      catchError(this.handleError)
-    );*/
-    return this.httpClient.post(this.baseUrl, data).pipe(
-      catchError(this.handleError)
-    );
+  create(data: any): Observable<any> {
+    return this.httpClient
+      .post(this.baseUrl, data)
+      .pipe(catchError(this.handleError));
   }
 
   // Edit / Update
   editCar(id: any, data: any): Observable<any> {
-    return this.httpClient.put(`${this.apiUrl}/${id}`, data).pipe(
-      catchError(this.handleError)
-    );
+    return this.httpClient
+      .put(`${this.baseUrl}/${id}`, data)
+      .pipe(catchError(this.handleError));
   }
 
   // Delete
   deleteCar(id: any): Observable<any> {
-    /*
-    return this.httpClient.delete(`${this.apiUrl}/${id}`).pipe(
-      catchError(this.handleError)
-    );*/
     const carUrl = `${this.baseUrl}/${id}`;
-    console.log(`aooo daje ${carUrl}`)
     return this.httpClient.delete(carUrl);
   }
 
@@ -72,20 +58,15 @@ export class CarService {
       console.error('An error occurred:', error.error.message);
     } else {
       console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
     }
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
-
-
-
+    return throwError('Something bad happened; please try again later.');
+  }
 }
 
 interface GetResponse {
   _embedded: {
-    cars: Car[]
-  }
+    cars: Car[];
+  };
 }
-
